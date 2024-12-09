36808995e9a09ae92dbaae6be005c1317bb6fe6d
pecolos <pecolos103@gmail.com>
2024-12-09 17:34:31 +0900

    Add files via upload

---
+++ kadai_005/bmi.js
@@ -0,0 +1,26 @@
+//体重
+let weight;
+
+weight = '体重';
+let weightNumber = 68 ;
+
+console.log(weight);
+console.log(weightNumber);
+
+//身長
+let height;
+
+height = '身長';
+let heightNumber = 1.7 ;
+
+console.log(height);
+console.log(heightNumber);
+
+//BMI
+let BMI;
+
+BMI = 'BMI';
+let BMINumber = weightNumber / (heightNumber*heightNumber);
+
+console.log(BMI);
+console.log(BMINumber);
